53f94d7195380f400669915ca0069c07c215f8bb
Alex Duller <alexduller@gmail.com>
2010-05-10 19:57:11 +0100

    Changed copyright and file title

--- site-lisp/themes/color-theme-alex.el
+++ site-lisp/themes/color-theme-alex.el
@@ -1,6 +1,7 @@
-;;;; color-theme-tango-2.el
+;;;; color-theme-alex.el
 ;; -*- mode: elisp -*-
 ;; Copyright (c) 2008-2009 Will Farrington <wcfarrington@gmail.com>
+;; Copyright (c) 2010 Alex Duller <alexduller@gmail.com>
 ;;
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as

66668c8e69c3c5f67c81052241f9cfa5e2e6c9b4
Alex Duller <alexduller@gmail.com>
2010-06-14 09:19:15 +0100

    Changed magit toggle section key to C-c TAB

--- init-git.el
+++ init-git.el
@@ -1,8 +1,6 @@
 (autoload 'magit-status "magit" "A git interface")
 (defun magit-tab ()
-  (local-set-key [tab] 'magit-toggle-section))
-
-(eval-after-load "magit"
-  (add-hook 'magit-mode-hook 'magit-tab))
+  (local-set-key (kbd "C-c TAB") 'magit-toggle-section))
+(add-hook 'magit-mode-hook 'magit-tab)
 
 (provide 'init-git)
